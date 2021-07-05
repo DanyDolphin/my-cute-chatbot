30d19a2b62fbe590d6121e48e2568fc9b977e165
DanyDolphin <danielcastillo59814@ciencias.unam.mx>
2021-07-05 17:26:42 -0500

    socket.io and dialogflow implemented

---
+++ README.md
@@ -0,0 +1,36 @@
+# My cute chatbot
+
+Cliente de chabot que manda y recibe mensajes de un agente de Dialogflow.
+
+## Requisitos
+
+1. Tener un agente de Dialogflow
+2. [Habilitar la API de Dialogflow en tu proyecto de Google Cloud](https://console.cloud.google.com/flows/enableapi?apiid=dialogflow.googleapis.com)
+3. [Crear una cuenta de servicio](https://cloud.google.com/docs/authentication/getting-started)
+
+## Instalación
+
+1. Instalar las dependencias de Node:
+```
+npm install
+```
+
+## Configuración
+
+1. Cambia el nombre del archivo `.env.sample` a `.env` y cambia los valores correspondientes:
+   1. `PORT`: puerto sobre el cual se iniciará el servidor de Express
+   2. `GOOGLE_PROJECTID`: el projectId de tu proyecto de google en el cual se encuentra tu agente de Dialogflow
+   3. `CONNECTION_MESSAGE`: El mensaje de bienvenida que le enviará el servidor al cliente cuando éste entre a la página de inicio
+2. Agrega en la raíz del proyecto tu archivo .json que descargaste cuando creaste la cuenta de servicio en Google Cloud. Cambiale el nombre a `google-credentials.json`
+
+## Correr el proyecto
+Si te encuentras en un ambiente de desarrollo, corre el siguiente comando:
+```
+npm run dev
+```
+Luego dirígete a `localhost:{PORT}/` (donde PORT es el puerto que elegiste para el servidor de Express). Verás la interfáz para comunicarte con tu chatbot.
+Si te encuentras en un ambiente de producción, corre el siguiente comando:
+```
+npm start
+```
+NOTA: el proyecto no leerá las variables de entorno en `.env` si el proyecto se encuentra en ambiente de producción
--- index.js
+++ index.js
@@ -1,13 +1,66 @@
+// express
 const express = require('express')
 const morgan = require('morgan')
 const path = require('path')
+const http = require('http')
 
 const app = express()
+const server = http.createServer(app)
 
-app.use(morgan('tiny'))
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
-
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.listen(3000, () => console.log('listening on port 3000'))
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('tiny'))
+  require('dotenv').config()
+}
+
+// dialogflow
+const dialogflow = require('@google-cloud/dialogflow')
+const uuid = require('uuid')
+
+async function queryChatbot(message) {
+  const sessionId = uuid.v4();
+
+  const sessionClient = new dialogflow.SessionsClient({
+    keyFilename: path.join(__dirname, 'google-credentials.json')
+  });
+  
+  const sessionPath = sessionClient.projectAgentSessionPath(
+    process.env.GOOGLE_PROJECTID,
+    sessionId
+  );
+
+  const request = {
+    session: sessionPath,
+    queryInput: {
+      text: {
+        text: message,
+        languageCode: 'es-MX',
+      },
+    },
+  };
+
+  // Send request and log result
+  const responses = await sessionClient.detectIntent(request);
+  const result = responses[0].queryResult;
+  return result.fulfillmentText
+}
+
+// socket.io
+const { Server } = require('socket.io')
+
+io = new Server(server)
+
+io.on('connection', socket => {
+  io.emit('chat message', process.env.CONNECTION_MESSAGE)
+
+  socket.on('chat message', message => {
+    queryChatbot(message).then(response => {
+      io.emit('chat message', response)
+    })
+  })
+})
+
+server.listen(process.env.PORT || 3000, () => console.log(`listening on port ${process.env.PORT}`))
